1b038669127dab131166bc81e56eb36d5d5eb7a8
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-12-17 15:17:33 +0200

    Ex1 Part3

--- Ex1/Ex1.cpp
+++ Ex1/Ex1.cpp
@@ -76,14 +76,71 @@ ostream& operator<< (ostream& out, const Project& p) {
 	return out;
 }
 
+class Directory {
+public:
+	Directory(const string& name)
+		: name(name)
+	{}
+
+	void add(const File& f) {
+		files.push_back(f);
+	}
+
+	string toString() const {
+		stringstream ss;
+		ss << name;
+		return ss.str();
+	}
+
+	string showContents(string sep = "") const {
+		if (files.size() == 0) {
+			return "";
+		}
+		stringstream ss;
+		ss << files[0];
+		for (size_t i = 1; i < files.size(); ++i) {
+			ss << sep << " " << files[i];
+		}
+		return ss.str();
+	}
+
+	double size() const {
+		double res = 0;
+		for (auto f : files) {
+			res += f.size();
+		}
+		return res;
+	}
+
+private:
+	string name;
+	vector<File> files;
+};
+
+
+ostream& operator<< (ostream& out, const Directory& p) {
+	out << p.toString();
+	return out;
+}
+
+void display(const Directory& d) {
+	cout << "Directory: " << d << endl;
+	cout << "File list:  " << d.showContents() << endl;
+	cout << "Size:  " << d.size() << " KB" << endl << endl;
+}
+
 int main()
 {
-	File ex1cpp("Ex1", "cpp", 17.3);
-	File ex1h("Ex1", "h", 5);
+	const File ex1cpp("Ex1", "cpp", 15);
+	const File maincpp("main", "cpp", 7);
+	const File ex1h("Ex1", "h", 5);
 
-	Project p("MyProj", 3, 8);
-	p.add(ex1cpp);
-	p.add(ex1h);
-	cout << p << endl;
+	Directory d("MyDir");
+	display(d);
+	d.add(maincpp);
+	d.add(ex1cpp);
+	d.add(ex1h);
+	display(d);
+	cout << "File list with comma: " << d.showContents(",") << endl;
 	return 0;
 }
