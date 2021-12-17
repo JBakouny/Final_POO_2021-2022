a66d9ef770c94a1e73c811da5c3cdc74b362bbe3
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-12-17 10:48:30 +0200

    Add project files.

---
+++ Ex1/Ex1.cpp
@@ -0,0 +1,45 @@
+﻿// Ex1.cpp : Defines the entry point for the application.
+//
+
+//#include "Ex1.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class File {
+
+public:
+	File(const string & name, 
+		const string & extension, 
+		double size)
+		: name(name), extension(extension), size(size)
+	{
+
+	}
+
+	string toString() const {
+		stringstream ss;
+		ss << "File: " << name << "." << extension << " (" << size << " KB)";
+		return ss.str();
+	}
+	ostream& toOstream(ostream& out) const {
+		return out
+	}
+private:
+	string name;
+	string extension;
+	double size;
+};
+
+ostream& operator<< (ostream& out, const File& f) {
+	out << f.toString();
+	return out;
+}
+
+int main()
+{
+	const File f("main", "cpp", 17.3);
+	cout << f.toString() << endl;
+	return 0;
+}
